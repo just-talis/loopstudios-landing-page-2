b053086177e94df2475bf26fc8249b6ca2ecfc6d
Esther Simon <esthersimon540@gmail.com>
2022-12-07 22:11:13 +0100

    update

---
+++ loopstudios-landingpage/src/App.js
@@ -0,0 +1,13 @@
+import Case from "./Components/Case";
+import Header from "./Components/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Case />
+    </>
+  );
+};
+
+export default App;
---
+++ loopstudios-landingpage/src/Components/Case.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Case = () => {
+  return (
+    <>
+    <section className='case'>
+        <div className='overlay'>
+            <h1>Immersive Experiences That Deliver</h1>
+        </div>
+    </section>
+    </>
+  )
+}
+
+export default Case
