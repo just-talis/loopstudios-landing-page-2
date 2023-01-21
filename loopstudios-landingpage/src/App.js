00b9157d38a0a2e268213f221da2f18e7d7ed42f
Esther Simon <esthersimon540@gmail.com>
2023-01-21 16:03:50 +0100

    desktop view

--- loopstudios-landingpage/src/App.js
+++ loopstudios-landingpage/src/App.js
@@ -2,8 +2,8 @@ import Case from "./Components/Case";
 import Creations from "./Components/Creations";
 import Header from "./Components/Header";
 import Interactive from "./Components/Interactive";
-import Footer from "./Components/Footer";
 import CreationsDesktop from "./Components/CreationsDesktop";
+import Footer from "./Components/Footer";
 
 const App = () => {
   return (
